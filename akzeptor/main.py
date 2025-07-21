d98c673fe2867060cffd39c75b8f6e750234fc06
cgloeckner <cgloeckner@freenet.de>
2025-07-21 21:38:41 +0200

    akzeptor standalone

--- akzeptor/main.py
+++ akzeptor/main.py
@@ -1,7 +1,42 @@
 import tkinter as tk
 from typing import Callable
 
-from shared.automata import State, StateMachine
+
+class State:
+    def __init__(self, name: str):
+        self.__name = name
+        self.final = False
+        self.__transitions: dict[str, State] = {}
+
+    def link(self, input: str, state: 'State') -> None:
+        if input in self.__transitions:
+            raise ValueError('Input transitions must be unique.')
+        
+        self.__transitions[input] = state
+
+    def __call__(self, input: str) -> 'State':
+        return self.__transitions[input]
+
+
+class StateMachine:
+    def __init__(self):
+        self.__states: dict[str, State] = {}
+
+    def add(self, name: str) -> 'State':
+        if name in self.__states:
+            raise ValueError('State names must be unique')
+    
+        self.__states[name] = State(name)
+        return self.__states[name]
+
+    def __call__(self, start: State, word: str) -> bool:
+        for input in word:
+            try:
+                start = start(input)
+            except KeyError:
+                return False
+        return start.final
+
 
 def on_button_click(entry: tk.Entry, test: Callable) -> None:
     word = entry.get()
